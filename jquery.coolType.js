c47e63c53e23f4387279cf910e4ab18b6dabbe06
Alex Ford <Alex.Ford@CodeTunnel.com>
2013-04-10 15:39:11 -0600

    Added character expansions.
    CoolType will now skip over strings found in the expansions array and will not try to type them out character by character.
    
    This allows you to use things like &nbsp; in strings passed to coolType. As long as &nbsp; is found in the expansions array (which it is by default) then it will insert &nbsp; as one character instead of 6.

--- jquery.coolType.js
+++ jquery.coolType.js
@@ -8,7 +8,14 @@
         delayAfterType: 3000,
         onComplete: false,
         onBeforeType: false,
-        onAfterType: false
+        onAfterType: false,
+        expansions: [
+            '&nbsp;',
+            '&gt;',
+            '&lt;',
+            '&quot;',
+            '&amp;'
+        ]
     };
 
     $.fn.coolType = function (text, options) {
@@ -35,11 +42,26 @@
             clearInterval($cursor.data('intervalId'));
         }
 
+        function expandChar(charIndex) {
+            var char = text[charIndex];
+            for (var expansionIndex in settings.expansions) {
+                var expansion = settings.expansions[expansionIndex];
+                if (expansion[0] === char) {
+                    var textToCompare = text.substr(charIndex, expansion.length);
+                    if (textToCompare === expansion)
+                        return { char: expansion, charIndex: charIndex + (expansion.length - 1) };
+                }
+            }
+            return { char: char, charIndex: charIndex };
+        }
+
         function typeText() {
             if (settings.onBeforeType) settings.onBeforeType();
             var charIndex = 0;
             var intervalId = setInterval(function () {
-                var char = text[charIndex];
+                var expanded = expandChar(charIndex),
+                    char = expanded.char;
+                charIndex = expanded.charIndex;
                 $container.append(char);
                 charIndex++;
                 if (charIndex === text.length) {
